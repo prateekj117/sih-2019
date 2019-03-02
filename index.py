870e77de812dac82dafcd6b0606255399ecfc9c4
Haider Ali <haider.lee23@gmail.com>
2019-03-03 02:01:21 +0530

    setup admin

--- index.py
+++ index.py
@@ -2,8 +2,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from flask import render_template
+from app import app, server
 from apps import home, gva_sectors, agg_national_accounts, gva_time_series, agg_eco_activities, household
+from apps.admin import requires_auth
 
 
 app.layout = html.Div([
@@ -11,6 +13,10 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
+@server.route('/admin')
+@requires_auth
+def admin():
+    return "Hello !"
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -27,6 +33,9 @@ def display_page(pathname):
         return agg_eco_activities.layout
     elif pathname == '/household':
         return household.layout
+    elif pathname == '/admin':
+        from apps import admin
+        return admin.layout
     else:
         return '404'
 
