4e79f6dab6c0d78fdcd46b253a5b0879e9f0f9f9
Roshan Sharma <reprogram46@gmail.com>
2019-03-02 20:38:07 +0530

    Add categories

--- apps/gva_time_series.py
+++ apps/gva_time_series.py
@@ -12,15 +12,18 @@ data = pd.read_excel('data/2018/economic-aggregates/S1.6.xlsx')
 years = data.iloc[5:6,2:-2]
 
 process = data[7:]
-
-# filtered = rows[0:-1] if remove else rows
+sections = process.iloc[:,0]
+main_sections = [index for index in sections.index if sections[index].isdigit()]
+rows = [data.iloc[idx] for idx in main_sections]
+labels = [row.iloc[-1] for row in rows]
+labelIds = [row.iloc[-2] for row in rows]
 
 layout = html.Div([
     html.H1('Stock Tickers'),
     dcc.Dropdown(
         id='my-dropdown',
-        options=[{'label': i, 'value': i} for i in process.iloc[:,-1]],
-        value=process.iloc[-1][-1],
+        options=[{'label': category, 'value': labelIds[idx]} for (idx, category) in enumerate(labels)],
+        value=labelIds[-1],
         style= {'margin-bottom': '20px'}
     ),
     dcc.Graph(id='my-graph',
@@ -32,7 +35,8 @@ layout = html.Div([
 @app.callback(Output('my-graph', 'figure'),
               [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
-    index = -1
+    print(selected_dropdown_value)
+    index = int(selected_dropdown_value)
     row = process.iloc[index, 2:-2].values
     year_list = ['Y ' + year for year in years.values[0]]
     mid = int(len(row)/2) 
