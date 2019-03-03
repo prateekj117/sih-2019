4f4d83a62a848955043392b3e1e62c2da759568a
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2019-03-03 13:05:18 +0530

    Add Data Tables in gcv (#41)

--- apps/gcf_time_series.py
+++ apps/gcf_time_series.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import math
+import dash_table
 
 from app import app
 import pandas as pd
@@ -15,9 +16,23 @@ process = data[7:]
 sections = process.iloc[:, 0]
 main_sections = [index for index in sections.index if str(sections[index]).isdigit() or (type(sections[index]) != str and math.isnan(sections[index]))]
 rows = [data.iloc[idx] for idx in main_sections]
-labels = [row.iloc[1] for row in rows]
+labels = [row.iloc[1] for row in rows[0:-1]]
 labelIds = main_sections
 print(labelIds[1])
+
+
+def generate_table(dataframe, max_rows=10):
+    data = pd.read_excel('data/2018/economic-aggregates/S1.10.xlsx', header = None)
+    df = data[6:]
+    df.columns = df.iloc[0].fillna(value=pd.Series(range(100)))
+    return(dash_table.DataTable(
+    data=df.to_dict('rows'),
+    columns=[{'id': c, 'name': c} for c in df.columns],
+    style_table={
+        'height': '400px',
+        'overflowY': 'scroll',
+        'border': 'thin lightgrey solid'
+    }))
 
 
 layout = html.Div([
@@ -29,8 +44,9 @@ layout = html.Div([
         style={'margin-bottom': '20px'}
     ),
     dcc.Graph(id='gcf-time-series',
-              style={'padding-top': '20px'})
-], className="container")
+              style={'padding-top': '20px'}),
+    generate_table(data)
+    ], className="container")
 
 
 @app.callback(Output('gcf-time-series', 'figure'),
