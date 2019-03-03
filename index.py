43ac8becdbbc45ed725d198687b179fb0254e620
Jeevanshi <itsjeevu@gmail.com>
2019-03-03 07:45:37 +0530

    Error

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import home, gva_sectors, agg_national_accounts, gva_time_series, agg_eco_activities, cfc_sectors, nv_eco, cfc_time_series, nv_time_series
+from apps import home, gva_sectors, agg_national_accounts, gva_time_series, agg_eco_activities, cfc_sectors, nv_eco, cfc_time_series, nv_time_series, gcf_sectors, gcf_time_series
 
 
 app.layout = html.Div([
@@ -19,6 +19,10 @@ def display_page(pathname):
         return home.layout
     elif pathname == '/agg_national_accounts':
         return agg_national_accounts.layout
+    elif pathname == '/gcf_sectors':
+        return gcf_sectors.layout
+    elif pathname == '/gcf_time_series':
+        return gcf_time_series.layout
     elif pathname == '/gva-sectors':
         return gva_sectors.layout
     elif pathname == '/gva-time-series':
